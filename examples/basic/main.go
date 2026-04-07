d6ccb5bf9d3a6dbebb6d1f7597bd28091066b1ca
SantianDev <cubexteams@gmail.com>
2026-04-07 19:14:36 +0300

    test

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -3,30 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/cubexteam/gomc-ping"
+	"github.com/cubexteam/gomc-ping/models"
 )
 
 func main() {
-	fmt.Println("--- gomc-ping Multi-Game Example ---")
+	host := "mc.hypixel.net"
+	port := uint16(25565)
 
-	// Universal Auto-Detection (works for any supported game)
-	mcHost := "play.hypixel.net"
-	resp, err := gomcping.Ping(mcHost, 25565)
-	if err == nil {
-		fmt.Printf("[Auto] Found %s: %s (Players: %d/%d)\n", resp.Edition, mcHost, resp.PlayersOn, resp.PlayersMax)
+	cfg := &models.Config{
+		Timeout: 5 * 1000 * 1000 * 1000,
+		SRV:     true,
 	}
 
-	// Explicit Game Ping
-	// Example for Rust (change IP to real one)
-	rustResp, err := gomcping.PingRust("1.2.3.4", 28015)
+	fmt.Printf("Pinging %s...\n", host)
+
+	resp, err := gomcping.PingWithConfig(host, port, cfg)
 	if err != nil {
-		fmt.Println("[Rust] Server offline or IP invalid")
-	} else {
-		fmt.Printf("[Rust] %s: %d players\n", rustResp.MOTD, rustResp.PlayersOn)
+		fmt.Printf("Error: %v\n", err)
+		return
 	}
 
-	// Saving Minecraft Favicon
-	if resp != nil && resp.Favicon != "" {
-		_ = gomcping.SaveFavicon(resp.Favicon, "favicon.png")
-		fmt.Println("[System] Minecraft favicon saved to favicon.png")
+	fmt.Println(resp.String())
+
+	if resp.Favicon != "" {
+		_ = gomcping.SaveFavicon(resp.Favicon, "hypixel_favicon.png")
+		fmt.Println("Favicon saved to hypixel_favicon.png")
 	}
 }
